f377c743bcc0df83e392b0adeec2ff5320fba06f
Jonathan I. Obise <obise.jonathan@gmail.com>
2020-07-02 19:01:00 +0100

    add setup.py file

---
+++ pypi_package/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='my_gaubi_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['my_gaubi_distributions'],
+      zip_safe=False)
